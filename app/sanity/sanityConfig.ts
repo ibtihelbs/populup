a29385fdf0fcbf5136b99e7ee13ece8bc609ee58
ibtihelbs <ibtihel.salah@outlook.fr>
2026-02-16 09:53:43 +0100

    config

--- app/sanity/sanityConfig.ts
+++ app/sanity/sanityConfig.ts
@@ -1,11 +1,14 @@
 import { createClient } from "@sanity/client";
 
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
+  throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
+}
+
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  dataset: "production",
-  apiVersion: "2024-01-01",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? "2024-01-01",
   useCdn: true,
-  token: process.env.SANITY_WRITE_TOKEN,
 });
 export const HOME_QUERY = `
 *[_type == "home"][0]
